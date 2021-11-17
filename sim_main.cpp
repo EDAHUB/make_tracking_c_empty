fea2a3722249e80b1ef6533094906835a9cef368
Marcel Chang <marcel.chang@shunyaocad.com>
2021-11-17 18:31:38 +0800

    add rand()

--- sim_main.cpp
+++ sim_main.cpp
@@ -21,6 +21,7 @@
 // Legacy function required only so linking works on Cygwin and MSVC++
 double sc_time_stamp() { return 0; }
 
+int my_rand() { return rand()%100; }
 int main(int argc, char** argv, char** env) {
     // This is a more complicated example, please also see the simpler examples/make_hello_c.
 
@@ -68,12 +69,13 @@ int main(int argc, char** argv, char** env) {
     top->in_wide[1] = 0x22222222;
     top->in_wide[2] = 0x3;
 
+    top->eval();
     // Marcel : decide time increasing for each step
     int time_incr_for_sub = 1;
 
+    int my_counter = 0;
     // Simulate until $finish
     while (!contextp->gotFinish()) {
-        contextp->timeInc(time_incr_for_sub);  // 1 timeprecision period passes...
 
         if (contextp->time() > 1 && contextp->time() < 10) {
             top->reset_l = !1;  // Assert reset
@@ -81,19 +83,19 @@ int main(int argc, char** argv, char** env) {
             top->reset_l = !0;  // Deassert reset
         }
 
-        top->in_small += 0x2;
+	my_counter ++;
 
-	top->top->sub->out_small += 1;
+        top->in_small = my_counter;
+
+	top->top->sub->out_small =  my_rand()<<8 | my_counter;
 
         top->eval();
 
 	// ==================================
 	// Marcel : collect sub docker data
 	// ==================================
-	unsigned char clk_docker = top->top->sub->clk;
-	unsigned char reset_l_docker = top->top->sub->reset_l;
-	unsigned int  in_small_docker = top->top->sub->in_small;
-	//printf ("[Collect sub docker data] TIME_INC=%d, clk =%d, reset=%d, in_small=%x\n",time_incr_for_sub, clk_docker,reset_l_docker,in_small_docker);
+	printf ("[Collect sub data] clk =%d, reset=%d, in_small=%x, out_small=%x\n",
+			top->top->sub->clk, top->top->sub->reset_l, top->top->sub->in_small, top->top->sub->out_small);
 	
 	
         top->clk = !top->clk;
@@ -103,6 +105,8 @@ int main(int argc, char** argv, char** env) {
         // Read outputs
         VL_PRINTF("[%" VL_PRI64 "d] clk=%x rstl=%x ismall=%x -> osmall=%x \n",
                   contextp->time(), top->clk, top->reset_l, top->in_small, top->out_small);
+        contextp->timeInc(time_incr_for_sub);  // 1 timeprecision period passes...
+
         if (contextp->time() > 50) { break; }
 
     }
